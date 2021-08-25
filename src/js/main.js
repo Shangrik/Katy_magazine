429a7da0cef6518f11f6d83aa899125d4fa41b71
Zaurbek Kasaev <Zaurik1983@inbox.ru>
2021-08-25 16:20:21 +0300

    Почти закончил промо

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,32 @@
-/**
-  * название функции
-  *
-  * @param {number} first - первое число
-  * @returns {number}
-  */
+const pulsbar = document.querySelector('.pulse');
+let on = false;
+
+let update = function() {
+  pulsbar.setAttribute('data-animation', (on) ? 'pulse' : '');
+  on = !(on);
+  setTimer();
+};
+
+let setTimer = function() {
+  setTimeout(function() {
+    requestAnimationFrame(update);
+
+  }, 1000);
+};
+setTimer();
+
+//lider 
+
+
+var slider = tns({
+  container: '.my-slider',
+  mode: "carousel",
+  items: 3,
+  slideBy: 1,
+  autoplay: true,
+  controls: false,
+  mouseDrag: true,
+  nav: false,
+  speed: 200
+});
+
