028dbed538996b55b45c750a97b57604a4dcff2c
Zaurbek Kasaev <Zaurik1983@inbox.ru>
2021-08-27 17:52:26 +0300

    almost done with site

---
+++ app/js/main1.js
@@ -0,0 +1,32 @@
+//lider 
+
+
+var slider = tns({
+    container: '.my-slider',
+    mode: "carousel",
+    items: 1,
+    slideBy: 1,
+    autoplay: true,
+    controls: false,
+    mouseDrag: true,
+    nav: false,
+    speed: 300,
+    preventScrollOnTouch: 'auto',
+    responsive: {
+      320: {
+        items: 1
+      },
+  
+      600: {
+        // edgePadding: 20,
+        items: 2
+      },
+     
+      900: {
+        items: 4,
+        autoWidth: true,
+        autoHeight: true
+      }
+    }
+  });
+  
